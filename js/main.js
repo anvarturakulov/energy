88b3e5f482c84d2da82eb2e00d373005a577a0b8
anvarturakulov <anvarsam@mail.ru>
2020-05-18 08:38:07 +0500

    added footer and services

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function(){
+
+    $(".services__itembox").slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: true,
+      arrows:false,
+    //   responsive: [
+    //     {
+    //       breakpoint: 1200,
+    //       settings: {
+    //         slidesToScroll: 3,
+            
+    //         slidesToShow: 3,
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 900,
+    //       settings: {
+    //         slidesToShow: 2,
+    //         slidesToScroll: 2
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 630,
+    //       settings: {
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1
+    //       }
+    //     }
+    //   ]
+    });
+
+//     $(".js-range-slider").ionRangeSlider({
+//       type: "double",
+//       min: 0,
+//       max: 1000,
+//       from: 0,
+//       to: 600,
+//       prefix: "$"
+//   });
+
+});
+
